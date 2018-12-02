b610fdc84dab4ae8c483ff175f670a4ae6d059aa
Clarencerainsford <45524772+Clarencerainsford@users.noreply.github.com>
2018-12-01 21:33:16 -0500

    Added hubert sheet

---
+++ Paul Riggio/Assets/Scripts/character_core.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+#Player determines if it's a human player or NPC, 0 = NPC | 1 = Human
+var player_type = 0
+var stats = {"Current_Health": 0, 
+             "Max_Health": 0, 
+             "Mana": 0, 
+             "Attack": 0,
+             "Attack_Speed": 0,
+             "Defense": 0,
+            }
+
+var possible_actions = {"Attack": false, "Pickup": false}
+
+func attack():
+	if possible_actions.Attack:
+		$Attack/CollisionShape2D.disabled = false
+		possible_actions.Attack = false
+		$AttackDuration.start()
+
+func hurt(damage_taken):
+	stats.Current_Health -= damage_taken
+	#print(stats.Current_Health)
+	#The area2D will be used as attack boxes
+	#When an enemy attack box is entered it calls this function with one argument
+
+func death():
+	queue_free()
