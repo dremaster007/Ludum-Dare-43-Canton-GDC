bdb95b7e5443e952c6401e4bdcc11125da7a006e
SUNY-GDC <45503453+SUNY-GDC@users.noreply.github.com>
2018-12-01 20:11:45 -0500

    NPC Patched

--- Raea Adams/Assets/Scripts/Attack.gd
+++ Raea Adams/Assets/Scripts/Attack.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
 var damage = 0
+var npc_type = 0
 
-func _on_NPC_Core_damage_transfer(new_damage):
+func _on_NPC_Core_damage_transfer(new_damage, npc):
 	damage = new_damage
+	npc_type = npc
+	if npc == 0:
+		add_to_group("enemy")
+	if npc == 1:
+		add_to_group("party")
