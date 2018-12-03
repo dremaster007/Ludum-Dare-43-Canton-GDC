f690f48c8666d65b3a0317dd7339ae751e4b8e32
Zachtoplasm <Zachtoplasm@gmail.com>
2018-12-02 19:57:45 -0800

    updated HUD and item pickups

--- Zach Samalot/Assets/Scripts/Dagger.gd
+++ Zach Samalot/Assets/Scripts/Dagger.gd
@@ -8,7 +8,11 @@ var Item
 var ItemClass
 
 var player
-#var HUD = get_parent().get_node("HUD")
+
+signal pickup
+signal item
+signal weapon
+
 
 func _ready():
 	ButtonScale = $ButtonPickup.rect_scale
@@ -20,50 +24,58 @@ func _ready():
 			Item = "Dagger"
 			ItemClass = "Weapon"
 			$Sprite.texture = load("res://Assets/Graphics/Weapons/dagger.png")
+			$ButtonPickup/ItemName.text = "Dagger"
 		
 		1:
 			#Sword Properties
 			Item = "Sword"
 			ItemClass = "Weapon"
 			$Sprite.texture = load("res://Assets/Graphics/Weapons/sword.png")
+			$ButtonPickup/ItemName.text = "Sword"
 		
 		2:
 			#Bow Properties
 			Item = "Bow"
 			ItemClass = "Weapon"
 			$Sprite.texture = load("res://Assets/Graphics/Weapons/bow.png")
+			$ButtonPickup/ItemName.text = "Bow"
 		
 		3:
 			#Staff Properties
 			Item = "Staff"
 			ItemClass = "Weapon"
 			$Sprite.texture = load("res://Assets/Graphics/Weapons/staff.png")
+			$ButtonPickup/ItemName.text = "Staff"
 			
 		4:
 			#Mana Potion Properties
 			Item = "ManaPot"
 			ItemClass = "Item"
 			$Sprite.texture = load("res://Assets/Graphics/Items/mana_potion.png")
+			$ButtonPickup/ItemName.text = "Mana Potion"
 			
 		5:
 			#Health Potion Properties
 			Item = "HealthPot"
 			ItemClass = "Item"
 			$Sprite.texture = load("res://Assets/Graphics/Items/health_potion.png")
+			$ButtonPickup/ItemName.text = "Health Potion"
 		
 func _process(delta):
 	pass
 	
 func buttonTween(appear):
 	if appear == 1:
-		$ButtonPickup.visible = true
+		$ButtonPickup.disabled = false
 		$ButtonPopup.interpolate_property($ButtonPickup, "modulate", Color(1,1,1,0), Color(1,1,1,1), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		$ButtonPopup.interpolate_property($ButtonPickup, "rect_scale", $ButtonPickup.rect_scale / 60, ButtonScale, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		$ButtonPopup.start()
 	else:
+		$ButtonPickup.disabled = true
 		$ButtonPopup.interpolate_property($ButtonPickup, "modulate", Color(1,1,1,1), Color(1,1,1,0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		$ButtonPopup.interpolate_property($ButtonPickup, "rect_scale", ButtonScale, $ButtonPickup.rect_scale / 60, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		$ButtonPopup.start()
+		
 	
 func _on_Dagger_body_entered(body):
 	if body.is_in_group("player"):
@@ -79,14 +91,6 @@ func _on_Dagger_body_exited(body):
 		buttonTween(0)
 
 
-func _on_ButtonPopup_tween_completed(object, key):
-	print("tween complete")
-
-
-func _on_ButtonPopup_tween_started(object, key):
-	print("tween started")
-
-
 func _on_ButtonPickup_pressed():
 	PickedUp = true
 	buttonTween(0)
@@ -95,11 +99,13 @@ func _on_ButtonPickup_pressed():
 		
 		"Weapon":
 			game_info.inventory.Weapon = Item
+			emit_signal("pickup", "weapon")
 			
 		
 		"Item":
 			game_info.inventory.Item = Item
-			#HUD.ItemCount += 1
+			emit_signal("pickup", "item")
+			
 		
 	
 	player.pickup()
