5f7e865bed74e962bc61dc64c7f11d8616d9c9c8
harchrisdytian <harchrisdytian@gmail.com>
2018-12-02 19:14:51 -0500

    copyed raea

---
+++ Christian Hardy/Assets/Scripts/game_info.gd
@@ -0,0 +1,33 @@
+extends Node
+
+#0 = Easy | 1 = Decent | 2 = Hard | 3 = Insane | 4 = Hell
+var current_difficulty = 0
+
+#This will hold the size of the party, it will start at 4 [The player and 3 party members]
+var party_size = 3
+
+#This holds a dictionary that has each difficulty which holds the values of the levels that can be pulled into the game
+var rooms = {"Easy": [], 
+             "Decent": [],
+             "Hard": [],
+             "Insane": [],
+             "Hell": []
+            }
+
+var character_texs = {"Raea": "res://Assets/Graphics/Characters/raea.png",
+                      "Hewer": "res://Assets/Graphics/Characters/hewer.png",
+                      "Emily": "Insert path",
+                      "Paul": "Insert path",
+                      "ZachL": "Insert path",
+                      "ZachS": "Insert path",
+                      "Tony": "Insert path",
+                      "James": "Insert path",
+                      "Karel": "Insert path",
+                      "Chris": "Insert path", 
+                    }
+
+var party = {"Player": character_texs.Raea,
+             "Member1": character_texs.Hewer,
+             "Member2": character_texs.Hewer,
+             "Member3": character_texs.Hewer,
+            }
